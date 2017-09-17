219f222ad886682e8059312a07ef06948919182c
ChandrasekharReddy <tchandu.soft@gmail.com>
2017-09-17 12:39:39 +0530

    Update notes.ts

--- Form/notes.ts
+++ Form/notes.ts
@@ -1,4 +1,12 @@
+app.component.html
+-----------------
+<form [formGroup]="myForm">
+      <panel-component [dynamicData]="dynamicData.panelData" [form]="myForm"></panel-component>
+</form>
 
+app.component.ts
+-----------------
+private myForm: FormGroup = new FormGroup({});
 
 constructor(private _fb: FormBuilder, private empService: EmpService) { }
   loadPositions() {
@@ -10,7 +18,6 @@ constructor(private _fb: FormBuilder, private empService: EmpService) { }
         console.log(error)
       });
   }
-
 
 buildForm(data) {
     //console.log(data);
@@ -27,4 +34,37 @@ buildForm(data) {
     });
     return this._fb.group(formGroup);
   };
+----------------------------
+  panel.component.ts:
+  
+  @Input() dynamicData: any;
+  @Input() form: FormGroup;
+  
+panel.component.html:
 
+<div [formGroup]="form">
+  <div *ngFor="let pd of dynamicData | orderby: 'PANEL_ORDER'" #parent>
+  <div *ngFor="let df of pd.PANEL_CONTENT  | orderby: 'order'" [ngClass]="{'col-md-12': pd.COMP_PER_ROW == 1,'col-md-6': pd.COMP_PER_ROW == 2, 'col-md-4': pd.COMP_PER_ROW == 3, 'col-md-3': pd.COMP_PER_ROW == 4, 'col-md-1': df.type =='label' &&  df.label == '(or)'}"
+            class="comp-space">
+   <div [ngSwitch]="df.type">
+        <div *ngSwitchCase="'textbox'" [style.width.%]="df.compWidth">
+          <textbox-component [dynamicData]="df" [form]="form"></textbox-component>
+        </div>
+   </div>
+  </div>
+  </div>
+</div>  
+------------------------
+text-box.component.ts:
+
+ @Input() dynamicData: any;
+ @Input() form: FormGroup;
+ private response: any = "";
+
+text-box.component.html:
+
+  <div [formGroup]="form">
+    <label for="">{{dynamicData.label}}</label>
+    <input type="text" class=" form-control" [formControlName]="dynamicData.name" [attr.name]="dynamicData.name" [(ngModel)]="response"
+    />
+</div>
