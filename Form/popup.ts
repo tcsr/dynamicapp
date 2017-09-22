df8e60fcf12dce087b30ba9cda67017b13798d80
ChandrasekharReddy <tchandu.soft@gmail.com>
2017-09-22 10:57:00 +0530

    Update popup.ts

--- Form/popup.ts
+++ Form/popup.ts
@@ -1,3 +1,6 @@
+
+https://hassantariqblog.wordpress.com/2017/03/22/angular2-using-custom-loader-spinner-as-service-in-angular-2-application/
+
 https://www.npmjs.com/package/ng2-bootstrap-modal
 
 npm install ng2-bootstrap-modal --save
@@ -12,4 +15,71 @@ imports: [BootstrapModalModule],
 declarations: [ConfirmComponent]
 
 -----------------
-confirm.component.ts
+confirm.component.ts :
+
+import { Component } from '@angular/core';
+import { DialogComponent, DialogService } from "ng2-bootstrap-modal";
+
+export interface ConfirmModel {
+  title:string;
+  message:string;
+}
+
+@Component({
+  selector: 'confirm',
+  template: `<div class="modal-dialog">
+                <div class="modal-content">
+                   <div class="modal-header">
+                     <button type="button" class="close" (click)="close()" >&times;</button>
+                     <h4 class="modal-title">{{title || 'Confirm'}}</h4>
+                   </div>
+                   <div class="modal-body">
+                     <p>{{message || 'Are you sure?'}}</p>
+                   </div>
+                   <div class="modal-footer">
+                     <button type="button" class="btn btn-primary" (click)="confirm()">OK</button>
+                     <button type="button" class="btn btn-default" (click)="cancel()">Cancel</button>
+                   </div>
+                 </div>
+                </div>`
+})
+export class ConfirmComponent extends DialogComponent<ConfirmModel, boolean> implements ConfirmModel {
+  title: string;
+  message: string;
+  constructor(dialogService: DialogService) {
+    super(dialogService);
+  }
+  confirm() {
+    // on click on confirm button we set dialog result as true,
+    // ten we can get dialog result from caller code
+    this.result = true;
+    this.close();
+  }
+  cancel() {
+    this.result = false;
+    this.close();
+  }
+}
+==============
+panel.component.ts :
+
+import { DialogService } from 'ng2-bootstrap-modal';
+import { ConfirmComponent } from './../popup/confirm.component'
+
+
+constructor(private dialogService: DialogService) { }
+
+showConfirm() {
+    this.dialogService.addDialog(ConfirmComponent, {
+      title: "Confirm",
+      message: "This is a Confirm dialog"
+    })
+      .subscribe((isConfirmed) => {
+        this.confirmResult = isConfirmed;
+        console.log(this.confirmResult)
+      })
+  };
+
+
+<button (click)="showConfirm()">Confirmation</button>
+
