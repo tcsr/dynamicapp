68bbcd38081ce2cb4e184dfade8074eddf19bcdb
ChandrasekharReddy <tchandu.soft@gmail.com>
2018-10-29 22:15:57 +0530

    Update login.ts

--- login.ts
+++ login.ts
@@ -161,6 +161,210 @@
   }
 
 
+----------------------------------------------------------------
+html
+---------------
+<div>
+  <mat-card>
+    <form [formGroup]="loginForm">
+      <mat-form-field class="full-width">
+        <label for="phoneNumber"></label>
+        <input type="text" phone id="phoneNumber" formControlName="phoneNumber" matInput placeholder="Phone Number" />
+      </mat-form-field>
+      <mat-form-field class="full-width">
+        <label for="ssn"></label>
+        <input [type]="showPassword ? 'text' : 'password'" ssn id="ssn" maxlength="11" formControlName="ssn" matInput placeholder="SSN"
+        />
+      </mat-form-field>
+      <button (click)="submit()">Submit</button>
+    </form>
+
+    <div>
+      <a href="javascript:void(0)" [class.adisabled]="showPassword == true" (click)="showPassword = true"> Show </a> |
+      <a href="javascript:void(0)" [class.adisabled]="showPassword == false" (click)="showPassword = false">
+        Hide </a>
+    </div>
+
+    <mat-card-content>
+      <pre>{{ arrayObject | groupBy: 'elm' | json }}</pre>
+      <div *ngFor="let obj of arrayObject | groupBy: 'elm'">
+        <!-- <div>{{obj.key}} has {{obj.value.length}} objects</div> -->
+        <mat-card>
+          <mat-card-header>
+            {{obj.key}}
+          </mat-card-header>
+          <mat-card-content>
+            <div *ngFor="let val of obj.value">
+              <mat-card>
+                <div>
+                  Id: {{val.id}}
+                </div>
+                <div>
+                  Element: {{val.elem}}
+                </div>
+                <div>
+                  Value: {{val.value}}
+                </div>
+              </mat-card>
+            </div>
+          </mat-card-content>
+        </mat-card>
+      </div>
+    </mat-card-content>
+  </mat-card>
+</div>
+----------------------------------------------------------------------------------------------------------
+ts
+-----------------------	
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  showPassword: boolean = false;
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
+  arrayObject = [
+    { id: 1, elm: 'foo', value: 0 },
+    { id: 2, elm: 'bar', value: 1 },
+    { id: 3, elm: 'foo', value: 2 },
+    { id: 4, elm: 'foo', value: 2 },
+    { id: 5, elm: 'bar', value: 5 },
+  ];
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      'phoneNumber': [''],
+      "ssn": ['', Validators.pattern(/^(?!219-09-9999|078-05-1120)(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}$/)]
+    })
+  }
+
+  submit() {
+    console.log(this.loginForm.controls);
+    this.loginForm.value['phoneNumber'] = this.loginForm.value['phoneNumber'].replace(/\D+/g, '');
+    this.loginForm.value['ssn'] = this.loginForm.value['ssn'].replace(/\D+/g, '');
+    console.log(this.loginForm.value);
+  }
+
+}
+
+
+----------------------------------------------------------------------------------------------------------
+scss
+-----
+a.adisabled {
+    cursor: not-allowed;
+    opacity: .65;
+    text-decoration: none;
+    color:#777;
+  }
+  
+  a.adisabled:hover { text-decoration: none; }
+  a.adisabled:focus { text-decoration: none; }
+  a.adisabled:active { text-decoration: none; }
+
+----------------------------------------------------------------------------------------------------------
+Groupby pipe:
+--------------
+
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({name: 'groupBy'})
+export class GroupByPipe implements PipeTransform {
+  transform(value: Array<any>, field: string): Array<any> {
+    const groupedObj = value.reduce((prev, cur)=> {
+      if(!prev[cur[field]]) {
+        prev[cur[field]] = [cur];
+      } else {
+        prev[cur[field]].push(cur);
+      }
+      return prev;
+    }, {});
+    return Object.keys(groupedObj).map(key => ({ key, value: groupedObj[key] }));
+  }
+}
+
+----------------------------------------------------------------------------------------------------------
+SSN Mask:
+---------
+
+import { Directive, Output, EventEmitter } from '@angular/core';
+import { NgControl } from '@angular/forms';
+
+@Directive({
+  selector: '[formControlName][ssn]',
+  host: {
+    '(ngModelChange)': 'onInputChange($event)',
+    '(keydown.backspace)': 'onInputChange($event.target.value,true)'
+  }
+})
+export class SSNMaskDirective {
+  constructor(public model: NgControl) { }
+  @Output() rawChange: EventEmitter<string> = new EventEmitter<string>();
+  onInputChange(event, backspace) {
+    var newVal = event.replace(/\D/g, '');
+    var rawValue = newVal;
+
+    if (backspace) {
+      newVal = newVal.substring(0, newVal.length);
+    }
+
+    if (newVal.length == 0) {
+      newVal = '';
+    }
+    else if (newVal.length <= 3) {
+      newVal = newVal.replace(/^(\d{0,3})/, '$1');
+    }
+    else if (newVal.length <= 6) {
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,2})/, '$1-$2');
+    }
+    else if (newVal.length <= 9) {
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,2})(\d{0,4})/, '$1-$2-$3')
+    }
+    this.model.valueAccessor.writeValue(newVal);
+    this.rawChange.emit(rawValue);
+  }
+}
+
+----------------------------------------------------------------------------------------------------------
+Phone Mask:
+-----------
+
+import { Directive, Output, EventEmitter } from '@angular/core';
+import { NgControl } from '@angular/forms';
+
+@Directive({
+  selector: '[formControlName][phone]',
+  host: {
+    '(ngModelChange)': 'onInputChange($event)'
+  }
+})
+export class PhoneMaskDirective {
+
+  constructor(public model: NgControl) { }
+
+  @Output() rawChange: EventEmitter<string> = new EventEmitter<string>();
+
+  onInputChange(e, backspace) {
+
+    var x = e.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+    e = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+
+    // set the new value
+    this.model.valueAccessor.writeValue(e);
+    this.rawChange.emit(e);
+  }
+
+}
+
+----------------------------------------------------------------------------------------------------------
+
 
 
 
